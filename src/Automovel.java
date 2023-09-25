d9f06d35efde30c542dec5f4c1d41cf8acce2751
Fernando Ferreira <ferreira2007ster@gmail.co>
2023-09-25 16:23:42 -0300

    tarefa de classes

---
+++ src/Automovel.java
@@ -0,0 +1,32 @@
+/**
+ * @author Fernando
+ */
+public class Automovel {
+    private String marca;
+    private String nome;
+    private int ano;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+}
